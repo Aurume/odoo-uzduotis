808ef9b31c6b025cf3c8808fb73b033326a34499
Aurume <51314673+Aurume@users.noreply.github.com>
2024-02-06 20:27:27 +0200

    Pirmas failas

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': "Dokumentai",        #modulio pavadinimas
+    'description': """Dokumentų valdymo programa""",
+    'author': "Aurelija",
+    'version': '0.0.1',
+    'depends': ['base'],
+
+}
