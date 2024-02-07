8429a5cd3216ac76b27e8593af26c7f64b256afa
Aurume <51314673+Aurume@users.noreply.github.com>
2024-02-07 08:33:55 +0200

    Update dokumentai.py

--- models/dokumentai.py
+++ models/dokumentai.py
@@ -9,5 +9,3 @@ class Dokumentai(models.Model):
     description = fields.Text(string="Aprašymas")
     company = fields.Char(string="Įmonė", required=True)
  #   company_id = fields.Many2one('res.company,')
- # created_by = fields.Many2one('res.users', string='Created by', default=lambda self: self.env.user, readonly=True)
-#     responsible_users = fields.Many2many('res.users', string='Responsible workers')
