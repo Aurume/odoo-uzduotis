473789ab19c4975de7e30c954ef01eb666f0d2e4
Aurume <51314673+Aurume@users.noreply.github.com>
2024-02-07 08:29:01 +0200

    Pataisytas

--- __manifest__.py
+++ __manifest__.py
@@ -4,5 +4,12 @@
     'author': "Aurelija",
     'version': '0.0.1',
     'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/dokumentai_view.xml',
+        'views/menu.xml',
+    ],
 
+    'application': True, # kad rodytų apps sąraše
+    'installable': True,
 }
